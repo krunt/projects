19f962752470f29e4daa2d1e29295e0d293b9b76
Kuts Alexey <akuts@yandex-team.ru>
2013-10-24 22:11:18 +0400

    fixed

--- misccode/bittorrent/src/utils.cc
+++ misccode/bittorrent/src/utils.cc
@@ -1,4 +1,5 @@
 #include <include/value.h>
+#include <boost/lexical_cast.hpp>
 
 namespace btorrent {
 
@@ -6,44 +7,42 @@ namespace detail {
 static std::string serialize_to_json(const value_t &v, int indent) {
     std::string result;
     switch (v.get_type()) {
-    case value_t::t_undef:     
-        throw bad_encode_decode_exception("can't serialize undef to json");
-    break;
     case value_t::t_integer:
-        result = boost::lexical_cast<value_t::string_type>(v.to_int());
+        result = std::string(indent, ' ') 
+            + boost::lexical_cast<value_t::string_type>(v.to_int());
     break;
     case value_t::t_string:     
-        result = "\"" 
-            + boost::lexical_cast<value_t::string_type>(v.to_string().size()) 
+        result = std::string(indent, ' ') + "\"" 
+            + v.to_string()
             + "\"";
     break;
     case value_t::t_list: {
         const value_t::list_type &lst = v.to_list();
-        result += std::string(indent, ' ') + "[\n";
+        result += "\n" + std::string(indent, ' ') + "[\n";
         for (int i = 0; i < lst.size(); ++i) {
-            result.append(detail::serialize_to_json(lst[i], 0));
-            if (i != lst.size()) { 
-                result += ",\n";
+            result.append(detail::serialize_to_json(lst[i], indent + 2));
+            if (i + 1 < lst.size()) { 
+                result += "\n";
             }
         }
-        result += std::string(indent, ' ') + "]\n";
+        result += "\n" + std::string(indent, ' ') + "]\n";
     break;
     }
     case value_t::t_dictionary: {
         const value_t::dictionary_type &mp = v.to_dict();
-        result += std::string(indent, ' ') + "{\n";
+        result += "\n" + std::string(indent, ' ') + "{\n";
         for (value_t::dictionary_type::const_iterator 
                 b = mp.begin(); b != mp.end();) 
         {
             const std::string &key = (*b).first;
-            result += "\"" + key + "\":";
-            result.append(detail::serialize_to_json((*b).second));
+            result += std::string(indent + 2, ' ') + "\"" + key + "\":";
+            result.append(detail::serialize_to_json((*b).second, indent + 2));
 
             if (++b != mp.end()) {
-                result += ",\n";
+                result += "\n";
             }
         }
-        result += std::string(indent, ' ') + "}\n";
+        result += "\n" + std::string(indent, ' ') + "}\n";
     break;
     }};
     return result;
